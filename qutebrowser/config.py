b3678289ed8771a5115648b7f156379fe3d43214
Dhruva Sagar <dhruva.sagar@gmail.com>
2020-05-23 13:41:14 +0530

    Add QuteBrowser
    Having used it in the past, this is a project that sees to have take
    really good shape. Nothing like being able to use a browser where vim
    like navigation isn't just a poor emulation

---
+++ qutebrowser/config.py
@@ -0,0 +1,11 @@
+import dracula.draw
+
+# Load existing settings made via :set
+config.load_autoconfig()
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
