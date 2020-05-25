abae00b696604fa70cfa25b217b6edd796135c78
Dhruva Sagar <dhruva.sagar@gmail.com>
2020-05-25 14:14:02 +0530

    Add alacritty & qutebrowser configs
    Switching to alacritty as my terminal and qutebrowser for my main
    browser

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -2,10 +2,13 @@ import dracula.draw
 
 # Load existing settings made via :set
 config.load_autoconfig()
+config.set('auto_save.session', True)
+config.bind(',m', 'hint links spawn mpv {hint-url}')
 
-dracula.draw.blood(c, {
-    'spacing': {
-        'vertical': 6,
-        'horizontal': 8
-    }
-})
+dracula.draw.blood(c)
+# , {
+#     'spacing': {
+#         'vertical': 6,
+#         'horizontal': 8
+#     }
+# })
