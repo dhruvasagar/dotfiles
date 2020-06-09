2df97cd840c6bf021ee5ab49a52c2c51cd70b63b
Dhruva Sagar <dhruva.sagar@gmail.com>
2020-06-09 22:02:02 +0530

    Updated qutebrowser configs

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -3,8 +3,9 @@ import dracula.draw
 # Load existing settings made via :set
 config.load_autoconfig()
 config.set('auto_save.session', True)
-config.set('window.hide_decoration', True)
+# config.set('window.hide_decoration', True)
 config.set('colors.webpage.prefers_color_scheme_dark', True)
+config.set('fonts.web.size.default', 14)
 
 config.bind(',m', 'spawn mpv {url}')
 config.bind(',M', 'hint links spawn mpv {hint-url}')
