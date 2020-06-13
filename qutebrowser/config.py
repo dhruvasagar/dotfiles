c3c9843cf2a9b1903bdf48336a0706301e7ce4d5
Dhruva Sagar <dhruva.sagar@gmail.com>
2020-06-13 16:13:10 +0530

    Updated configs

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,7 +1,7 @@
 import dracula.draw
 
 # Load existing settings made via :set
-config.load_autoconfig()
+# config.load_autoconfig()
 config.set('auto_save.session', True)
 # config.set('window.hide_decoration', True)
 config.set('colors.webpage.prefers_color_scheme_dark', True)
