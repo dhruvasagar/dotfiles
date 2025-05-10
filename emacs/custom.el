da87786b86175bece6a59f2009ef62b73251123e
Dhruva Sagar <dhruva.sagar@gmail.com>
2025-05-10 15:09:08 +0530

    Add separate emacs configurations for customizations

---
+++ emacs/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-check-simple-format t nil nil "Customized with use-package doom-modeline")
+ '(safe-local-variable-values
+   '((blacken-executable
+      . "/Users/dhruva/.local/share/virtualenvs/qualitycontrol-eqFOpcz7/bin/black")
+     (pyvenv-virtual-env-name . "qualitycontrol-eqFOpcz7")
+     (pyvenv-virtual-env
+      . "/Users/dhruva/.local/share/virtualenvs/qualitycontrol-eqFOpcz7/")
+     (python-pytest-executable . "docker-compose exec app pytest"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
